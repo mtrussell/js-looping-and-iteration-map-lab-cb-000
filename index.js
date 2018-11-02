202fdb6cd074a2911b04a37b4388c9828a1c7002
Matt Trussell <smtrussell@gmail.com>
2018-11-02 22:29:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file.
+
+function lowerCaseDrivers(oldNames){
+  const lowNames = oldNames.map(function(name){
+    return name.toLowerCase();
+  });
+  return lowNames;
+}
+
+
+function nameToAttributes(oldNames) {
+  const propNames = oldNames.map(function(name){
+    const nameSplit = name.split(' ');
+    return Object.assign({}, {firstName: nameSplit[0], lastName: nameSplit[1]});
+  });
+  return propNames;
+}
+
+
+function attributesToPhrase(hometownDrivers){
+  const hometownPhrase = hometownDrivers.map(function(driver){
+    return `${driver.name} is from ${driver.hometown}`;
+  });
+  return hometownPhrase;
+}
