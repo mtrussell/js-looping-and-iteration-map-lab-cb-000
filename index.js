a6f78c74ddd8bcbcc05ec14a0f6f313eb300ee6c
Matt Trussell <smtrussell@gmail.com>
2018-11-02 22:21:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 // Code your solution in this file.
 
-function lowerCaseDrivers(){
-
+function lowerCaseDrivers(oldNames){
+  const lowNames = oldNames.map(function(name){
+    return name.toLowerCase();
+  });
 }
 
 
 function nameToAttributes(oldNames) {
   const propNames = oldNames.map(function(name){
     const nameSplit = name.split(' ');
-    return Object.assign({}, {firstName: firstName, lastName: lastName});
+    return Object.assign({}, {firstName: nameSplit[0], lastName: nameSplit[1]});
   });
+}
+
+
+function attributesToPhrase(){
+  
 }
