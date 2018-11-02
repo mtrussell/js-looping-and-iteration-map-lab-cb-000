3f53ce39f00f032bca586fcac777fdff35f68052
Matt Trussell <smtrussell@gmail.com>
2018-11-02 22:28:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function nameToAttributes(oldNames) {
 
 function attributesToPhrase(hometownDrivers){
   const hometownPhrase = hometownDrivers.map(function(driver){
-    return `#{driver.name} is from #{driver.hometown}`;
+    return `${driver.name} is from ${driver.hometown}`;
   });
   return hometownPhrase;
 }
